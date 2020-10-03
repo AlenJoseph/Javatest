dc0011cfcc32350321b6a0879ec63a46f8718d08
Alen Joseph <alenjoseph333@gmail.com>
2020-10-03 12:10:05 +0530

    Update DiscountRate.java

--- DiscountRate.java
+++ DiscountRate.java
@@ -3,9 +3,12 @@ public class DiscountRate {
     private static double serviceDiscountPremium = 0.2;
     private static double serviceDiscountGold = 0.15;
     private static double serviceDiscountSilver = 0.1;
+    private static double productDiscountNone = 0.0;
     private static double productDiscountPremium = 0.1;
     private static double productDiscountGold = 0.1;
     private static double productDiscountSilver = 0.1;
+    private static double serviceDiscountNone = 0.0;
+    
 
     public static double getServiceDiscountRate(String type) {
         switch (type) {
@@ -15,6 +18,8 @@ public class DiscountRate {
                 return serviceDiscountGold;
             case "Silver":
                 return serviceDiscountSilver;
+            case "None":
+                return serviceDiscountNone;
             default:
                 throw new IllegalArgumentException("wrong service type specified");
         }
@@ -28,6 +33,8 @@ public class DiscountRate {
                 return productDiscountGold;
             case "Silver":
                 return productDiscountSilver;
+            case "None":
+                return productDiscountNone;
             default:
                 throw new IllegalArgumentException("wrong service type specified");
         }
